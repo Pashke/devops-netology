e160b24af0c4de76547b27018a61cf41f899c1de
Pavel <19tipok88@gmail.com>
2022-07-03 22:00:32 +0700

    Домашнее задание к занятию "7.4. Средства командной работы над инфраструктурой"

---
+++ homework/07-terraform-04-teamwork/terraform/node01.tf
@@ -0,0 +1,27 @@
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${var.centos-7-base}"
+      type     = "network-nvme"
+      size     = "50"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ homework/07-terraform-04-teamwork/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "yandex_cloud_id" {
+  default = "b1grcpn8qi84n3n3a607"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gbgo924r85q278bg78"
+}
+
+variable "centos-7-base" {
+  default = "fd8hqa9gq1d59afqonsf"
+}
+
+variable "token" {
+  type = string
+  sensitive = true
+}
